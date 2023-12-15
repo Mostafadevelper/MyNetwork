34406abf22292fbe3a2df9f8ff038834e028e0ce
Mostafa mahmoud <mostafa.maabdelgawad@orange.com>
2023-12-15 23:05:04 +0200

    Init app

---
+++ MyNetwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNetwork
+//
+//  Created by Mostafa on 15/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
